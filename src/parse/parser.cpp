d5ee11fb8bcefc66cbad99563e5317371358b0ce
Aryan Agarwal <aryan.agarwal2983@gmail.com>
2026-02-17 12:01:29 +0530

    added backslash outside quotes

--- src/parse/parser.cpp
+++ src/parse/parser.cpp
@@ -9,24 +9,32 @@ std::vector<std::string> Parser::split(const std::string& s){
     std::string cur;
     bool quote = false;
     bool dquote = false;
+    bool back = false;
 
     for(char ch : s){
         // handle space
-        if(!dquote && !quote && ch == ' '){
+        if(!back && !dquote && !quote && ch == ' '){
             if(!cur.empty()){
                 out.push_back(cur);
                 cur.clear();
             }
         }
-        //handle quotes
-        else if(!dquote && ch == '\''){
+        // handle quotes
+        else if(!back && !dquote && ch == '\''){
             quote = !quote;
         }
-        else if(!quote && ch == '\"'){
+        else if(!back && !quote && ch == '\"'){
             dquote = !dquote;
+        }
+        // handle back slash outside quotes
+        else if(!back && !dquote && !quote && ch == '\\'){
+            back = !back;
         }
         else {
             cur.push_back(ch);
+            if(back){
+                back = !back;
+            }
         }
     }
 
